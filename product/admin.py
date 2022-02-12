4c5ed42c6ee66194759404a7b44857610653e878
maa1379 <79393100+maa1379@users.noreply.github.com>
2022-02-13 02:10:01 +0330

    Update admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import ExportalProduct, InternalProduct, ProductBase, Internal_Image, Exportal_Image ,Loaded,LinedProduct,LinedProduct,Rejected
-
+from import_export.admin import ExportActionMixin
 # Register your models here.
-admin.site.register(ExportalProduct),
+class BookAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('slate_code',)
+admin.site.register(BookAdmin,ExportalProduct),
 admin.site.register(InternalProduct),
 admin.site.register(ProductBase),
 admin.site.register(Internal_Image),
