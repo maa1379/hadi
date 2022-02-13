c461357610137b53a47b5bb716870c749ac937f4
maa1379 <79393100+maa1379@users.noreply.github.com>
2022-02-13 05:24:29 +0330

    Update admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import ExportalProduct, InternalProduct, ProductBase, Internal_Image, Exportal_Image ,Loaded,LinedProduct,LinedProduct,Rejected
-
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
+
+@admin.register(Rejected)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
 admin.site.register(ExportalProduct),
 admin.site.register(InternalProduct),
 admin.site.register(ProductBase),
@@ -10,4 +14,4 @@ admin.site.register(Internal_Image),
 admin.site.register(Exportal_Image),
 admin.site.register(Loaded),
 admin.site.register(LinedProduct),
-admin.site.register(Rejected),
+# admin.site.register(Rejected),
